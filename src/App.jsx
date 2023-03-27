91222ec206497cc48466f80e159cd4ac628800af
iboterhoek <iboterhoek@idea11.com.au>
2023-03-27 13:57:03 +1000

    added portfolio project, fixed up scrolling and set clear to refresh page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import './App.css';
 import { useState, useRef } from "react";
-import $ from "jquery";
-
-let terminal = []
 
 function ReturnOutputs(outputs) {
 	let divs = []
@@ -15,7 +12,7 @@ function ReturnOutputs(outputs) {
 	return divs
 }
 
-function ReturnTerminal() {
+function ReturnTerminal(terminal) {
 	let divs = []
 	for (let i = 0; i < terminal.length; i++) {
 		divs.push(
@@ -40,13 +37,24 @@ let projects = [
 			"		Then this is the website for you! Just refresh the page and bam. Ready to go password.",
 			"		All you need to do is click on the text and it puts it right in your clipboard."
 		]
-	}
+	},
+	{
+		"name": "Portfolio",
+		"command": "portfolio",
+		"url": "https://github.com/indyboterhoek/portfolio",
+		"description": [
+			"		Would you like to show off your work to the world?",
+			"		Are you an avid linux user and want to show off your skills?",
+			"		Then this is the website for you! Just update the projects array and bam. Ready to go portfolio.",
+			"		Then you can just use the command 'project {projectCommand}' to open the project in a new tab."
+		]
+	},
 ]
 
 function App() {
 	const [input, setInput] = useState("");
 	const [spin, setSpin] = useState(false);
-	const [shouldClear, setClear] = useState(false);
+	const [terminal, setTerminal] = useState([]);
 	const ref = useRef();
 
 	let commands = [
@@ -54,7 +62,8 @@ function App() {
 			"name": "clear",
 			"description": "clears the terminal back to its original state",
 			"func": function() {
-				terminal = [];
+				setTerminal([]);
+				window.location.reload();
 			}
 		},
 		{
@@ -120,6 +129,7 @@ function App() {
 				let newOutputs = [];
 				let cmds = input.split(" ");
 				let commandFound = false;
+				let tempTerminal = terminal;
 
 				for (let i = 0; i < commands.length; i++) {
 					if (commands[i]["name"] === cmds[0] && !commandFound) {
@@ -133,12 +143,15 @@ function App() {
 					newOutputs.push("Command Not Found. Please use the help command to show a list of commands.");
 				}
 
-				terminal.push({
+				tempTerminal.push({
 					"input": input,
 					"outputs": newOutputs
 				})
 
+				setTerminal(tempTerminal);
+
 				setInput("");
+				ref.current.focus();
 			}
 			if (e.key === "Tab") {
 
@@ -158,17 +171,37 @@ function App() {
 			</div>
 			<br/>
 			Thank you for visiting my website. Feel free to look around. If you get stuck remember to use the <b className="command" onClick={e => {
-				setInput("help")
-				ref.current.focus()
-				$(".text-input").keydown(function() {
-					
+				let newOutputs = [];
+				let commandFound = false;
+				let tempTerminal = terminal
+				
+				for (let i = 0; i < commands.length; i++) {
+					if (commands[i]["name"] === "help" && !commandFound) {
+						commandFound = true
+						newOutputs = commands[i].func()
+					}
+				}
+				
+				tempTerminal.push({
+					"input": "help",
+					"outputs": newOutputs
 				})
+
+				setTerminal(tempTerminal)
+
+				setInput("help");
+
+				setTimeout(() => {
+					setInput("");
+					ref.current.focus();
+				}, 100)
+
 			}}>help</b> command.
 			<br/>
 			<br/>
 		</div>
 		<div className="terminal">
-			{ReturnTerminal()}
+			{ReturnTerminal(terminal)}
 			<p className="account">indy@indyboterhoek.com</p>:<font color="red">~</font>$ {}
 		</div>
 		<input ref={ref} className="text-input" autoFocus type="text" value={input} onChange={e => setInput(e.target.value.toLowerCase())} onKeyDown={handleSubmit} onFocus={e => {  }}/>
